01de06309c4b08dfe1450c06537ae5788983029b
Sergey Akopov <info@sergeyakopov.com>
2016-06-14 12:30:16 -0500

    Update pack path in the build script.

--- Build.ps1
+++ Build.ps1
@@ -59,4 +59,4 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet pack .\source\WebApi.JSend -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\source -c Release -o .\artifacts --version-suffix=$revision }
